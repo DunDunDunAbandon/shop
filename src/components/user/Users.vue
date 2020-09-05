af8bc3d7f3d5ca28029bfa78b2eb4166aa9c9d22
DunDunDunAbandon <1928820709@qq.com>
2020-09-05 21:50:04 +0800

    用户管理还差一点儿功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -12,12 +12,12 @@
       <!--搜索与添加区域-->
       <el-row :gutter="20">
         <el-col :span="7">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary">添加用户</el-button>
+          <el-button type="primary" @click="addDialogVisible=true">添加用户</el-button>
         </el-col>
       </el-row>
       <!--用户列表-->
@@ -30,28 +30,125 @@
         <el-table-column label="状态">
           <template v-slot="scope">
             <el-switch
-              v-model="scope.row.mg_state">
+              v-model="scope.row.mg_state" @change="userStateChanged(scope.row)">
             </el-switch>
           </template>
         </el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <!--此处这个width没有实现响应式布局-->
+        <el-table-column label="操作" width="200">
+          <template v-slot="scope">
+            <!--修改-->
+            <el-tooltip effect="dark" content="修改" placement="top" :enterable="false">
+              <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            </el-tooltip>
+            <!--删除-->
+            <el-tooltip effect="dark" content="删除" placement="top" :enterable="false">
+              <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            </el-tooltip>
+            <!--分配角色-->
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
+      <!--分页区域-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
     </el-card>
+    <!--添加用户的对话框-->
+    <el-dialog
+      title="添加用户"
+      :visible.sync="addDialogVisible"
+      width="30%"
+      @close="addDialogClosed">
+      <!--内容主题区域-->
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <!--底部区域-->
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="addDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+  </span>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
   export default {
     data(){
+      //验证邮箱的规则
+      var checkEmail=(rule,value,cb)=>{
+        //验证邮箱的正则表达式
+        const regEmail=/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/;
+        if(regEmail.test(value)){
+          //合法的邮箱
+          return cb()
+        }
+        cb(new Error('请输入合法的邮箱'))
+      };
+      //验证手机号的规则
+      var checkMobile=(rule,value,cb)=>{
+        const regMobile=/^(0|86|17951)?(13[0-9]|15[0123456789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
+        if(regMobile.test(value)){
+          return cb()
+        }
+        cb(new Error('请输入合法的手机号'))
+      };
       return {
         //获取用户列表的参数对象
         queryInfo:{
           query:'',
-          pagenum:1,
-          pagesize:2
+          pagenum:1,//当前的页数
+          pagesize:2//当前每页显示多少条数据
         },
         userlist:[],
-        total:0
+        total:0,
+        addDialogVisible:false,//控制添加用户对话框的显示与隐藏
+        addForm:{
+          username:'',
+          password:'',
+          email:'',
+          mobile:''
+        },//添加用户的表单数据
+        addFormRules:{//添加表单的验证规则对象
+          username:[
+            {required:true,message:'请输入用户名',trigger:'blur'},
+            {min:3,max:10,message:'用户名的长度在3-10个字符之间',trigger:'blur'}
+          ],
+          password:[
+            {required:true,message:'请输入密码',trigger:'blur'},
+            {min:6,max:15,message:'密码的长度在3-10个字符之间',trigger:'blur'}
+          ],
+          email:[
+            {required:true,message:'请输入邮箱',trigger:'blur'},
+            {validator:checkEmail,trigger:'blur'}
+          ],
+          mobile:[
+            {required:true,message:'请输入手机',trigger:'blur'},
+            {validator:checkMobile,trigger:'blur'}
+          ]
+        },
       }
     },
     created(){
@@ -67,6 +164,29 @@
         this.total=res.data.total;
 
         console.log(res)
+      },
+      //监听pagesize改变的事件
+      handleSizeChange(newSize){
+        this.queryInfo.pagesize=newSize;
+        this.getUserList()
+      },
+      //监听页码值改变的事件
+      handleCurrentChange(newPage){
+        this.queryInfo.pagenum=newPage;
+        this.getUserList()
+      },
+      // 监听switch状态的改变
+      async userStateChanged(userinfo){
+        const {data:res}=await this.$http.put(`users/${userinfo.id}/state/${userinfo.mg_state}`);
+        if(res.meta.status!==200){
+          userinfo.mg_state=!userinfo.mg_state;
+          return this.$message.error('更新用户状态失败！')
+        }
+        this.$message.success('更新用户状态成功！')
+      },
+      // 监听添加用户对话框的关闭事件，重置表单
+      addDialogClosed(){
+        this.$refs.addFormRef.resetFields();
       }
     }
   }
