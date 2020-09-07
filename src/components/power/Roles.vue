a1523ef823f1498cee19363bfa24f1a9e48a6c03
DunDunDunAbandon <1928820709@qq.com>
2020-09-07 17:25:05 +0800

    角色管理

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -19,18 +19,35 @@
         <!--展开列-->
         <el-table-column type="expand">
           <template v-slot="scope">
-            <el-row :class="['bdbottom',i1===0?'bdtop':'']" v-for="(item1,i1) in scope.row.children" :key="item1.id">
+            <el-row :class="['bdbottom',i1===0?'bdtop':'','vcenter']" v-for="(item1,i1) in scope.row.children" :key="item1.id">
               <!--渲染一级权限-->
               <el-col :span="5">
-                <el-tag>{{item1.authName}}</el-tag>
+                <el-tag closable
+                        @close="removeRightById(scope.row,item1.id)">{{item1.authName}}</el-tag>
                 <i class="el-icon-caret-right"></i>
               </el-col>
               <!--渲染二级和三级权限-->
-              <el-col :span="19"></el-col>
+              <el-col :span="19">
+                <!--通过for循环嵌套渲染二级权限-->
+                <el-row :class="[i2===0?'':'bdtop','vcenter']" v-for="(item2,i2) in item1.children" :key="item2.id">
+                  <el-col :span="6">
+                    <el-tag type="success"
+                            closable
+                            @close="removeRightById(scope.row,item2.id)">{{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag type="warning"
+                            v-for="(item3,i3) in item2.children"
+                            :key="item3.id"
+                            closable
+                            @close="removeRightById(scope.row,item3.id)">
+                      {{item3.authName}}
+                    </el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
             </el-row>
-            <pre>
-              {{scope.row}}
-            </pre>
           </template>
         </el-table-column>
         <!--索引列-->
@@ -41,13 +58,34 @@
           <template v-slot="scope">
             <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
             <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
-            <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
+            <el-button size="mini" type="warning" icon="el-icon-setting" @click="showSetRightDialog(scope.row)">分配权限</el-button>
           </template>
         </el-table-column>
         <el-table-column label="角色名称" prop="roleName"></el-table-column>
 
       </el-table>
     </el-card>
+    <!--分配权限的对话框-->
+    <el-dialog
+      title="分配权限"
+      :visible.sync="setRightDialogVisible"
+      width="50%"
+      @close="setRightDialogClosed">
+      <!--树形控件-->
+      <el-tree
+        show-checkbox
+        :data="rightslist"
+        :props="treeProps"
+        node-key="id"
+        default-expand-all
+        :default-checked-keys="defKeys"
+        ref="treeRef">
+      </el-tree>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="setRightDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="allotRights">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -56,7 +94,20 @@
     data(){
       return {
         //所有角色列表数据
-        roleList:[]
+        roleList:[],
+        //控制分配权限对话框的显示与隐藏
+        setRightDialogVisible:false,
+        //所有权限的数据
+        rightslist:[],
+        //树形控件的属性绑定对象
+        treeProps:{
+          label:'authName',
+          children:'children'
+        },
+        //默认选中的结点id值数组
+        defKeys:[],
+        //当前即将分配权限的角色id
+        roleId:''
       }
     },
     created(){
@@ -71,6 +122,70 @@
         }
         this.roleList=res.data
         console.log(res)
+      },
+      //根据id删除对应的权限
+      async removeRightById(role,rightId){
+        //弹框提示用户是否决定删除
+        const confirmResult=await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).catch(err=>err);
+
+        if(confirmResult!=='confirm'){
+          return this.$message.info('取消了删除！')
+        }
+        const {data:res}=await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+        if(res.meta.status!==200){
+          return this.$message.error('删除权限失败！')
+        }
+        // 防止整个列表的刷新，提升用户体验
+        role.children=res.data
+      },
+      //展示分配权限的对话框
+      async showSetRightDialog(role){
+        this.roleId=role.id;
+        //获取所有权限的数据
+        const {data:res}=await this.$http.get('rights/tree')
+        if(res.meta.status!==200){
+          return this.$message.error('获取权限数据失败！')
+        }
+        //把获取到的权限数据保存到data中
+        this.rightslist=res.data;
+        console.log(this.rightslist);
+        //递归获取三级节点的id
+        this.getLeafKeys(role,
+        this.defKeys)
+        this.setRightDialogVisible=true
+      },
+      //通过递归的形式获取角色下所有三级权限的id，并保存到defkeys数组中
+      getLeafKeys(node,arr){
+        //如果当前node结点不包含children属性则是三级节点
+        if(!node.children){
+          return arr.push(node.id)
+        }
+        node.children.forEach(item=>
+        this.getLeafKeys(item,arr))
+      },
+      //监听分配权限对话框的关闭事件
+      setRightDialogClosed(){
+        this.defKeys=[]//及时清空数组防止保存的id越来越多
+      },
+      //点击为角色分配权限
+      async allotRights(){
+        const keys=[
+          //展开运算符,将一个数组转为用逗号分隔的参数序列
+          ...this.$refs.treeRef.getCheckedKeys(),
+          ...this.$refs.treeRef.getHalfCheckedKeys()
+        ]
+        const idStr=keys.join(',')
+        const {data:res}=await this.$http.post(`roles/${this.roleId}/rights`,{rids:idStr})
+        if(res.meta.status!==200){
+          return this.$message.error('分配权限失败！')
+        }
+        this.$message.success('分配权限成功！')
+        this.getRolesList();
+        this.setRightDialogVisible=false
       }
     }
   }
@@ -86,5 +201,9 @@
   }
   .bdbottom{
     border-bottom:1px solid #eee
+  }
+  .vcenter{
+    display:flex;
+    align-items: center;
   }
 </style>
